8dba4288d17a22ca879e67617f3fa5d0f6bfac42
Gustavo Gracio <gustavogracio@hotmail.com>
2021-07-28 16:07:32 -0300

    Exercicio Introduçao a Javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-Javascript-Primeiros-passos/script.js
@@ -0,0 +1,13 @@
+const myName = "Gustavo";
+const birthCity = "Mauá";
+let birthYear = 2002;
+
+birthYear = 2030;
+
+birthCity = "Ribeirão Pires";
+
+// O erro acontece porque o valor atribuido a uma constante (const) não pode ser alterado.
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
