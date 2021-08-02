0cd79c7e6b685993158e9f3078652db84f58fe37
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-02 19:17:17 -0300

    Adiciona exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
@@ -21,3 +21,22 @@ for (let key in info){
 for (let key in info){
     console.log(info[key]);
 }
+
+// Exercicio 5
+let secondeInfo = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
+
+for(let key in info){
+    if (info[key] === secondeInfo[key]){
+        console.log("Ambos recorrentes");
+    }else{
+        console.log(info[key] + " e " + secondeInfo[key]);
+    }
+
+}
+
+
