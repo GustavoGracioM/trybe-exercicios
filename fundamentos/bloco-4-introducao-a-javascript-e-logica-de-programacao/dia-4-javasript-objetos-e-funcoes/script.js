439c1923f8299dc75e0fb7d3c2956651076b0110
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-02 14:53:42 -0300

    Adiciona exercicio 5 para fixar

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
@@ -18,3 +18,6 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
 //Exercicio 4
 console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo " + player.bestInTheWorld.length + " vezes.");
+
+//Exercico 5
+console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.")
