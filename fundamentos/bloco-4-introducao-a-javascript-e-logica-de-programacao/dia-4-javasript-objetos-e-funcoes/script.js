0006cd9557542cf3bbe95791aacb6204d689200d
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-02 15:10:42 -0300

    Adiciona exercicio 2 para fixar parte II

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
@@ -33,3 +33,14 @@ let names = {
 for (let key in names){
     console.log("Olá, " + names[key]);
 }
+
+//Exercicio 2
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for (let key in car){
+    console.log(key, car[key]);
+}
