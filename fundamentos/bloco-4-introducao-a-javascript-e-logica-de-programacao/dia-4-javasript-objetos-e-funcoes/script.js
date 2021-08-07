e5f21e3eeb57c6db09d2869b90bf0d67998b3f44
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-07 19:42:23 -0300

    Mensagem sobre a alteração feita

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javasript-objetos-e-funcoes/script.js
@@ -50,4 +50,102 @@ function palindromo(string){
 }
 palindromo("ovo");
 
+// Exercicio 2
+let higherNumber = [500, 3, 6, 700, 100, 1]; 
+function maiorIndice(array){
+  for (let key1 = 0; key1 < array.length; key1 += 1){
+    let contador = 0;
+      for (let key2 = 0; key2 < array.length; key2 += 1){
+          if (array[key1] > array[key2]){
+            contador += 1;
+          }
+      }
+    
+    if (contador === array.length - 1){
+        console.log(array[key1]);
+    }
+  }
+}
+maiorIndice(higherNumber);
+
+// Exercicio 3
+let smallerNumber = [50, 30, 6, 7, 10, 100]; 
+
+function menorIndice(array){
+    for (let key1 = 0; key1 < array.length; key1 += 1){
+        let contador = 0;
+          for (let key2 = 0; key2 < array.length; key2 += 1){
+              if (array[key1] < array[key2]){
+                contador += 1;
+              }
+          }
+        
+        if (contador === array.length - 1){
+            console.log(array[key1]);
+        }
+      }    
+}
+menorIndice(smallerNumber);
+
+// Exercicio 4
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+function maiorNome(nome){
+    for (let key1 = 0; key1 < nome.length; key1 += 1){
+        let contador = 0;
+        for (let key2 = 0; key2 < nome.length; key2 += 1){
+             if(nome[key1].length > nome[key2].length){
+                 contador += 1;
+           }
+        }
+        if (contador === nome.length - 1){
+            console.log(nome[key1]);
+        }
+    }
+}
+maiorNome(nomes);
+
+// Exercicio 5
+let repete = [2, 3, 2, 5, 8, 2, 3];
+function recorrente(array){
+    for (let key1 = 0; key1 < array.length; key1 += 1){
+        let contador = 0;
+        for (let key2 = 0; key2 < array.length; key2 +=1){
+            if (array[key1] === array[key2]){
+                contador += 1;
+                console.log(array[key1])
+            }
+        }
+        if ( contador )
+        if (contador === array.length ){
+            console.log(array[key1]);
+        }
+    }
+}
+recorrente(repete);
+
+// Exercicios 6
+let somatorio = 5;
+let arrayDeNumeros = [];
+let resultado = 0;
+function somaDeTudo(numero){
+    for (let soma = 1; soma <= numero; soma += 1){
+        arrayDeNumeros.push(soma);
+        resultado += soma
+    }
+    console.log(arrayDeNumeros.join('+') + " = " + resultado);
+}
+somaDeTudo(somatorio);
+
+// Exercicio 7
+let word = "trybe";
+let ending = "be";
+function finalDaOutra(inicio, fim){
+    let tamanho = Math.trunc(inicio.length / 2);
+    if(inicio.slice(-tamanho) === fim){
+        return console.log(true);
+    }else{
+        return console.log(false);
+    }
+}
+finalDaOutra(word, ending);
 
