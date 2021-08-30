9a0959a89a0212620accdfdb9bf89f38486b6d87
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-30 14:49:40 -0300

    Adiciona exercicio 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -35,15 +35,19 @@ const order = {
 };
 
 const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
   const result =  `Olá ${order.order.delivery.deliveryPerson} entrega para: ${order.name}, elefone ${order.phoneNumber}, R. ${order.address.street}, Nº:${order.address.number}, AP:${order.address.apartment}`;
   console.log(result);
 }
-  // Adicione abaixo as informações necessárias.
-
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+  // Adicione abaixo as informações necessárias.  "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+  order.name = 'Luiz Silva';
+  order.order.pizza.marguerita.price = 20;
+  const pagamento = order.order.pizza.marguerita.price + order.order.pizza.pepperoni.price + order.order.drinks.coke.price;
+  const result = `Ola ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é RS ${pagamento},00 `;
+  console.log(result);
 
 }
 
