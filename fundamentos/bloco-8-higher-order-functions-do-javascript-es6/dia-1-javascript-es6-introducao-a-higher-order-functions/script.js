fa63e72bda80d36c926b1c17b93cda4981eef2cb
Gustavo Gracio <gustavogracio@hotmail.com>
2021-09-02 15:53:11 -0300

    Adiciona exercicio 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -19,3 +19,29 @@ const randomNumber = () => Math.floor((Math.random() * 5) + 1);
 const comparar = (num1, num2) => num1 === num2;
 const verfica = (selected, random) => comparar(selected, random) ? 'Parabéns você ganhou' : 'Tente novamente';
 console.log(verfica(3, randomNumber));
+
+// Exercicio 3
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+
+const resultProva = (gabarito, prova, action) => {
+  let nota = 0;
+  for (let key = 0; key < gabarito.length; key += 1) {
+    const check = action(gabarito[key], prova[key]);
+    nota += check;
+  }
+  return `Você acertou ${nota} questões`;
+}
+
+console.log(resultProva(RIGHT_ANSWERS, STUDENT_ANSWERS, (num1, num2) => {
+  if (num2 !== 'N.A') {
+    if (num1 === num2) {
+      return 1;
+    } else {
+      return -0.5;
+    }
+  }
+  return 0;
+}));
+    
