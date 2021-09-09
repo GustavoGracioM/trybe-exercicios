0552f02fad727c7105acb3f389643c98f3f455e9
Gustavo Gracio <gustavogracio@hotmail.com>
2021-09-09 18:13:08 -0300

    Adiciona exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -4,7 +4,7 @@ const names = [
   'Aanemarie', 'Adervandes', 'Akifusa',
   'Abegildo', 'Adicellia', 'Aladonata',
   'Abeladerco', 'Adieidy', 'Alarucha',
-];
+]; 
 
 function containsA() {
   // escreva seu código aqui
