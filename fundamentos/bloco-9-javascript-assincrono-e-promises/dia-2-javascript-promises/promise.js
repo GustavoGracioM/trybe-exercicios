55d87c5dd4e85da4fb70765879be40f0e61ff453
Gustavo Gracio <gustavogracio@hotmail.com>
2021-09-16 14:34:39 -0300

    Adiciona todos os exercicios bloco 9 dia 9.2

---
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/promise.js
@@ -0,0 +1,14 @@
+const fetchPromise = () => {
+  const promise = new Promise((resolve, reject) => {
+    const array = Array.from({ length: 10 }, () =>  Math.floor(Math.random() * 50) + 1);
+    const sum = array.reduce((acc, curr) => acc + curr, 0);
+    (sum < 8000) ? resolve(sum) : reject();
+  });
+  const div = [2, 3, 4, 5];
+  promise
+    .then((sum) => div.map((number) => sum / number))
+    .then((allSum) => allSum.reduce((acc, curr) => acc + curr, 0 ))
+    .catch(() => console.log('É mais de oito mil!'));
+};
+
+fetchPromise();
