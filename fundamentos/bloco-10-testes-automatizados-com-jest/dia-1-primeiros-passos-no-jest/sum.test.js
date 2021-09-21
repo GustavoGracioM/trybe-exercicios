de00ff900ef8fbb1506fe9fd5ea888dc9c32d340
Gustavo Gracio <gustavogracio@hotmail.com>
2021-09-21 15:27:52 -0300

    Adiciona exericicios bloco 10 dia 10.1 exercicio 1

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/sum.test.js
@@ -0,0 +1,26 @@
+// loginValidation.test.js
+const { test, expect } = require("@jest/globals");
+const sum = require("./sum.js");
+
+describe("a função verifyCredentials()", () => {
+
+  test('Verifica se 4 + 5 é 9', () => {
+    expect(sum(4, 5)).toEqual(9);
+  });
+
+  test('Verifica se 4 + 5 é 9', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+
+  test('Verifica se o valor passado é um numero', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  test('Verifica se o erro retornado é "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow('parameters must be numbers');
+  });
+  });
