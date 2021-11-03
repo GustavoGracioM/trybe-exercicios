90caa539d63daa45dfb6fd829fad7c8c2b11b5cd
Gustavo Gracio <gustavogracio@hotmail.com>
2021-11-03 18:18:42 -0300

    Adiciona exercicios bloco 14 dia 14.2 exercicio 2

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/jokes/src/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/jokes/src/service.test.js
@@ -11,4 +11,13 @@ describe("Exercicios dia 14.2", () => {
     expect(service.randomNumber).toBeCalledTimes(1);
   });
 
+  it('Exercicio 2', () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b) => a / b);
+    
+    expect(service.randomNumber(10, 2)).toBe(5);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(10, 2);
+  })
+
 });
